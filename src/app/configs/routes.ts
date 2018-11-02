4d82c66321adb8cac25e4fafceeeeea9184d3780
Jade L <jadellanes97@gmail.com>
2018-11-02 18:28:59 +0100

    home

--- src/app/configs/routes.ts
+++ src/app/configs/routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { NotFoundPageComponent } from '@app/core/components/not-found-page/not-found-page.component';
+import {HomePageComponent} from '@app/core/components/home-page/home-page.component';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: NotFoundPageComponent },
+    { path: 'home', component: HomePageComponent },
     { path: 'login', component: NotFoundPageComponent },
     { path: 'conversation',      component: NotFoundPageComponent },
     { path: '',
